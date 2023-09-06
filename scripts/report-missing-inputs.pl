42dc35749970ae61ab0d80c230401f4608ea8d1e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-06 14:19:53 -0400

    report-missing-inputs: Handle codependents

--- scripts/report-missing-inputs.pl
+++ scripts/report-missing-inputs.pl
@@ -14,9 +14,11 @@ sub get_from_json_env {
     return $result;
 }
 
+my @codependent = @{ get_from_json_env 'CODEPENDENT' };
 my %inputs = %{ get_from_json_env 'INPUTS' };
 
 my @required;
+my $status = 0;
 
 open INPUT, '<', "$ENV{GITHUB_ACTION_PATH}/action.yml";
 while (<INPUT>) {
@@ -34,8 +36,30 @@ while (<INPUT>) {
     }
 }
 
+if (@codependent && %inputs) {
+    for my $dependents (@codependent) {
+        my $expected = 0;
+        my $found = 0;
+        my @needed;
+        my @dependents = @{$dependents};
+        for $dependent (@dependents) {
+            ++$expected;
+            if (defined $inputs{$dependent} && $inputs{$dependent}) {
+                ++$found;
+            } else {
+                push @needed, $dependent;
+            }
+        }
+        if ($found && $expected != $found) {
+            print "::error ::Codependent inputs (".join(', ', @dependents).") need: ".join(', ', @needed)."\n";
+            $status |= 2;
+        }
+    }
+}
+
 if (@required) {
     print "::error ::Some required inputs are missing\n";
     print '::notice ::Required inputs: ['.join(', ', @required)."]\n";
+    $status |= 1;
 }
-exit 1;
+exit $status;
